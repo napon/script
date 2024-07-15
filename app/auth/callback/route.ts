04bf8eb08fc52ca1521003dcf432160ff17ad534
Phil Chan <35395991+hungrypc@users.noreply.github.com>
2024-07-14 22:37:09 -0400

    adjust config

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   // by the SSR package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get("code")
+  const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin
 
   if (code) {

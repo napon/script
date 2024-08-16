c13a07023586b42503a7e91745fb4e146a80cb4c
brihui <brianhui94@gmail.com>
2024-08-16 15:02:29 -0700

    Resized things based on Andrew's feedback

--- app/project/left-bar.tsx
+++ app/project/left-bar.tsx
@@ -3,17 +3,17 @@ import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 export function LeftBar() {
   return (
     <ResizablePanelGroup direction="vertical" className="space-y-1">
-      <ResizablePanel defaultSize={40} className="border p-1">
+      <ResizablePanel defaultSize={30} className="border p-1">
         <div>Authors</div>
       </ResizablePanel>
-      <ResizablePanel defaultSize={40} className="border p-1">
+      <ResizablePanel defaultSize={15} className="border p-1">
         <div>Keywords</div>
       </ResizablePanel>
-      <ResizablePanel defaultSize={10} className="border p-1">
-        <div>Target Journal</div>
+      <ResizablePanel defaultSize={30} className="border p-1">
+        <div>Citations</div>
       </ResizablePanel>
-      <ResizablePanel defaultSize={10} className="border p-1">
-        <div>Export Button</div>
+      <ResizablePanel defaultSize={25} className="border p-1">
+        <div>Notes</div>
       </ResizablePanel>
     </ResizablePanelGroup>
   )
--- app/project/right-bar.tsx
+++ app/project/right-bar.tsx
@@ -1,15 +1,23 @@
+// import { Button } from "@/components/ui/button"
 import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 
 export function RightBar() {
   return (
     <ResizablePanelGroup direction="vertical" className="space-y-1">
-      <ResizablePanel className="border p-1">
-        <div>Citations</div>
+      <ResizablePanel defaultSize={5}>
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel defaultSize={25} className="border p-1">
+            <div>Export</div>
+          </ResizablePanel>
+          <ResizablePanel defaultSize={75} className="border p-1">
+            <div>Target Journal</div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </ResizablePanel>
-      <ResizablePanel className="border p-1">
+      <ResizablePanel defaultSize={42.5} className="border p-1">
         <div>Figures Import</div>
       </ResizablePanel>
-      <ResizablePanel className="border p-1">
+      <ResizablePanel defaultSize={42.5} className="border p-1">
         <div>Figures Preview</div>
       </ResizablePanel>
     </ResizablePanelGroup>
